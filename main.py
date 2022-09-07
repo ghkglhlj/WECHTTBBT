de63d4a07d98d296ea7c11acf2b32cca6856d7a6
ghkglhlj <112951286+ghkglhlj@users.noreply.github.com>
2022-09-07 19:18:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None) 
 STRING = os.environ.get("STRING", None) 
 MONGO_URL = os.environ.get("MONGO_URL", None)
+ERROR_LOG = os.environ.get("ERROR_LOG", None)
 
 bot = Client(STRING, API_ID, API_HASH)
 
