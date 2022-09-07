52e6568a8b14a94e286674ee290ecfe1c4bd2609
ghkglhlj <112951286+ghkglhlj@users.noreply.github.com>
2022-09-07 17:42:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ async def is_admins(chat_id: int):
 
 @bot.on_message(filters.command("start"))
 async def start(client, message):
-        await message.reply_text("Hi! My name is HNY. I'm an Artificial Intelligence")
+        await message.reply_text("Hi! My name is Nethya I'm an Artificial Intelligence")
 
 
 @bot.on_message(
